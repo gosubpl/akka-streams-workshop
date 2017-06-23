c68ba9c210a186caadaea2295f098d7ce998d1d2
gosubpl <github@gosub.pl>
2017-06-23 13:22:15 +0200

    Streams working

--- src/test/scala/pl/gosub/akka_streams/workshop/HelloSpec.scala
+++ src/test/scala/pl/gosub/akka_streams/workshop/HelloSpec.scala
@@ -1,10 +1,18 @@
 package pl.gosub.akka_streams.workshop
 
+import akka.NotUsed
+import akka.actor.ActorSystem
+import akka.stream.ActorMaterializer
+import akka.stream.scaladsl.Source
 import org.scalatest.FreeSpec
 
 class HelloSpec extends FreeSpec {
   "A friendly greeter" - {
+    implicit val system = ActorSystem("Examples")
+    implicit val materializer = ActorMaterializer()
     "should say hello world!" in {
+      val source: Source[Int, NotUsed] = Source(1 to 100)
+      source.runForeach(i => println(i))(materializer)
       assert("hello world" == "hello world")
     }
   }
