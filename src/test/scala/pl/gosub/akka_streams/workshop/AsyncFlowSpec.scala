0f0b8a113ede252e4238c78b80778411c4b56877
gosubpl <github@gosub.pl>
2017-07-01 16:26:29 +0200

    formatting

--- src/test/scala/pl/gosub/akka_streams/workshop/AsyncFlowSpec.scala
+++ src/test/scala/pl/gosub/akka_streams/workshop/AsyncFlowSpec.scala
@@ -3,8 +3,8 @@ package pl.gosub.akka_streams.workshop
 import akka.NotUsed
 import akka.actor.ActorSystem
 import akka.event.Logging
-import akka.stream.{ActorAttributes, ActorMaterializer, ActorMaterializerSettings, Supervision}
 import akka.stream.scaladsl.{Flow, Sink, Source}
+import akka.stream.{ActorAttributes, ActorMaterializer, ActorMaterializerSettings, Supervision}
 import org.scalatest.FreeSpec
 
 import scala.concurrent.ExecutionContext.Implicits.global
@@ -16,7 +16,9 @@ import scala.util.Try
 // Some examples adapted from there
 
 class AsyncFlowSpec extends FreeSpec {
+
   import FutureExtensions._
+
   "A Flow with custom materializer should" - {
     implicit val system = ActorSystem("SimpleFlowSpec")
 
@@ -32,148 +34,153 @@ class AsyncFlowSpec extends FreeSpec {
     "finish without failure for materializer with custom decider" in {
       val decider: Supervision.Decider = {
         case _: ArithmeticException => Supervision.Resume
-        case _                      => Supervision.Stop
+        case _ => Supervision.Stop
       }
       implicit val materializer = ActorMaterializer(ActorMaterializerSettings(system).withSupervisionStrategy(decider))
       val source = Source(0 to 5).map(100 / _)
       val stream = source.runWith(Sink.fold(0)(_ + _))
       stream.awaitOnCompleteAndPrint
     }
+  }
 
-    "A Flow with standard materializer should" - {
-      implicit val system = ActorSystem("SimpleFlowSpec")
-      implicit val materializer = ActorMaterializer()
+  "A Flow with standard materializer should" - {
+    implicit val system = ActorSystem("SimpleFlowSpec")
+    implicit val materializer = ActorMaterializer()
 
-      "finish without failure for custom decider attached at flow level" in {
-        val decider: Supervision.Decider = Supervision.resumingDecider // always Resume
-        val source: Source[Int, NotUsed] = Source(0 to 10)
-        val flow: Flow[Int, Int, NotUsed] = Flow[Int].map(100 / _).withAttributes(ActorAttributes.supervisionStrategy(decider))
-        val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
-        stream.awaitOnCompleteAndPrint
-      }
+    "finish without failure for custom decider attached at flow level" in {
+      val decider: Supervision.Decider = Supervision.resumingDecider // always Resume
+      val source: Source[Int, NotUsed] = Source(0 to 10)
+      val flow: Flow[Int, Int, NotUsed] = Flow[Int].map(100 / _).withAttributes(ActorAttributes.supervisionStrategy(decider))
+      val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
+      stream.awaitOnCompleteAndPrint
+    }
 
-      // what strategies can we choose?
-      /*
-        Stop - The stream is completed with failure. [Default decider]
-        Resume - The element is dropped and the stream continues.
-        Restart - The element is dropped and the stream continues after restarting the stage.
-                  Restarting a stage means that any accumulated state is cleared.
-                  This is typically performed by creating a new instance of the stage.
+    // what strategies can we choose?
+    /*
+      Stop - The stream is completed with failure. [Default decider]
+      Resume - The element is dropped and the stream continues.
+      Restart - The element is dropped and the stream continues after restarting the stage.
+                Restarting a stage means that any accumulated state is cleared.
+                This is typically performed by creating a new instance of the stage.
 
-        difference between Resume and Restart can be seen e.g. in Scan operation
-       */
+      difference between Resume and Restart can be seen e.g. in Scan operation
+     */
 
-      // scan
-      "adjust strategy for flow using Scan depending on decider" in {
-        // what is Scan ? it is online fold, i.e.
-        Source(1 to 7).scan(1)(_ * _).log("elementStream") // http://doc.akka.io/docs/akka/current/scala/stream/stages-overview.html#log
-          .withAttributes(ActorAttributes.logLevels(onElement = Logging.ErrorLevel)) // log each element at ERROR level
-          .runWith(Sink.foreach(println)).awaitOnComplete(_ => println("Scan run completed"))
+    // scan
+    "adjust strategy for flow using Scan depending on decider" in {
+      // what is Scan ? it is online fold, i.e.
+      Source(1 to 7).scan(1)(_ * _).log("elementStream") // http://doc.akka.io/docs/akka/current/scala/stream/stages-overview.html#log
+        .withAttributes(ActorAttributes.logLevels(onElement = Logging.ErrorLevel)) // log each element at ERROR level
+        .runWith(Sink.foreach(println)).awaitOnComplete(_ => println("Scan run completed"))
 
-        // now let's see how it behaves with default decider (that is Stop)
-        val source: Source[Int, NotUsed] = Source(0 to 20)
-        val flow: Flow[Int, Int, NotUsed] = Flow[Int].map {
-          i =>  if (i % 10 == 0) {
+      // now let's see how it behaves with default decider (that is Stop)
+      val source: Source[Int, NotUsed] = Source(0 to 20)
+      val flow: Flow[Int, Int, NotUsed] = Flow[Int].map {
+        i =>
+          if (i % 10 == 0) {
             throw new IllegalArgumentException("don't like numbers divisble by 10")
           } else {
             100 / i
           }
-        }.scan(0)(_ + _)
-        val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
-        stream.awaitOnCompleteAndPrint
+      }.scan(0)(_ + _)
+      val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
+      stream.awaitOnCompleteAndPrint
 
-        // with resuming decider
-        val flow2 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.resumingDecider))
-        val stream2 = source.via(flow2)
-          .runWith(Sink.foreach(println))
-        stream2.awaitOnCompleteAndPrint
+      // with resuming decider
+      val flow2 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.resumingDecider))
+      val stream2 = source.via(flow2)
+        .runWith(Sink.foreach(println))
+      stream2.awaitOnCompleteAndPrint
 
-        // with restarting decider
-        val flow3 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider))
-        val stream3 = source.via(flow3)
-          .runWith(Sink.foreach(println))
-        stream3.awaitOnCompleteAndPrint
+      // with restarting decider
+      val flow3 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider))
+      val stream3 = source.via(flow3)
+        .runWith(Sink.foreach(println))
+      stream3.awaitOnCompleteAndPrint
 
-        // why no difference? because Map was first impacted by the resume strategy...
-        // so Scan is getting only nice numbers...
-      }
+      // why no difference? because Map was first impacted by the resume strategy...
+      // so Scan is getting only nice numbers... as we will see later on
+    }
 
-      "adjust strategy for Scan stage depending on decider" in {
-        // now let's see how it behaves with default decider (that is Stop)
-        val source: Source[Int, NotUsed] = Source(0 to 20)
-        val flow: Flow[Int, Int, NotUsed] = Flow[Int].scan(0) {
-          (acc, el) =>  if (el % 10 == 0) {
-              throw new IllegalArgumentException("don't like numbers divisble by 10")
-            } else {
+    "adjust strategy for Scan stage depending on decider" in {
+      // now let's see how it behaves with default decider (that is Stop)
+      val source: Source[Int, NotUsed] = Source(0 to 20)
+      val flow: Flow[Int, Int, NotUsed] = Flow[Int].scan(0) {
+        (acc, el) =>
+          if (el % 10 == 0) {
+            throw new IllegalArgumentException("don't like numbers divisble by 10")
+          } else {
             acc + (100 / el)
-            }
-        }
-        val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
-        stream.awaitOnCompleteAndPrint
-
-        // with resuming decider
-        val flow2 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.resumingDecider))
-        val stream2 = source.via(flow2)
-          .runWith(Sink.foreach(println))
-        stream2.awaitOnCompleteAndPrint
-
-        // with restarting decider
-        val flow3 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider))
-        val stream3 = source.via(flow3)
-          .runWith(Sink.foreach(println))
-        stream3.awaitOnCompleteAndPrint
+          }
       }
+      val stream = source.via(flow).runWith(Sink.fold(0)(_ + _))
+      stream.awaitOnCompleteAndPrint
 
-      "compare the internals of both cases with log" in {
-        val source: Source[Int, NotUsed] = Source(0 to 5)
-        val flowA: Flow[Int, Int, NotUsed] = Flow[Int].map {
-          i =>  if (i % 5 == 0) {
+      // with resuming decider
+      val flow2 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.resumingDecider))
+      val stream2 = source.via(flow2)
+        .runWith(Sink.foreach(println))
+      stream2.awaitOnCompleteAndPrint
+
+      // with restarting decider
+      val flow3 = flow.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider))
+      val stream3 = source.via(flow3)
+        .runWith(Sink.foreach(println))
+      stream3.awaitOnCompleteAndPrint
+    }
+
+    "compare the internals of both cases with log" in {
+      val source: Source[Int, NotUsed] = Source(0 to 5)
+      val flowA: Flow[Int, Int, NotUsed] = Flow[Int].map {
+        i =>
+          if (i % 5 == 0) {
             throw new IllegalArgumentException("don't like numbers divisble by 5")
           } else {
             100 / i
           }
-        }.log("element stream 1").scan(0)(_ + _).log("scan stream 1")
+      }.log("element stream 1").scan(0)(_ + _).log("scan stream 1")
 
-        val flowB: Flow[Int, Int, NotUsed] = Flow[Int].scan(0) {
-          (acc, el) =>  if (el % 10 == 0) {
+      val flowB: Flow[Int, Int, NotUsed] = Flow[Int].scan(0) {
+        (acc, el) =>
+          if (el % 10 == 0) {
             throw new IllegalArgumentException("don't like numbers divisble by 10")
           } else {
             acc + (100 / el)
           }
-        }.log("scan stream 2")
+      }.log("scan stream 2")
 
-        // be extra careful to set attributes only in one place
-        // additional attributes to be added using the .and combinator :)
-        // otherwise they will override
-        
-        // with restarting decider
-        val flow1 = flowA.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider)
-        .and(ActorAttributes.logLevels(onElement = Logging.ErrorLevel))  )
-        val stream1 = source.via(flow1)
-          .runWith(Sink.ignore)
-        stream1.awaitOnCompleteAndPrint
+      // be extra careful to set attributes only in one place
+      // additional attributes to be added using the .and combinator :)
+      // otherwise they will override
 
-        // with restarting decider
-        val flow2 = flowB.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider)
-          .and(ActorAttributes.logLevels(onElement = Logging.ErrorLevel))  )
-        val stream2 = source.via(flow2)
-          .runWith(Sink.ignore)
-        stream2.awaitOnCompleteAndPrint
-      }
+      // with restarting decider
+      val flow1 = flowA.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider)
+        .and(ActorAttributes.logLevels(onElement = Logging.ErrorLevel)))
+      val stream1 = source.via(flow1)
+        .runWith(Sink.ignore)
+      stream1.awaitOnCompleteAndPrint
 
-      // mapAsync / mapAsyncUnordered / scanAsync - retry/supervision strategies
+      // with restarting decider
+      val flow2 = flowB.withAttributes(ActorAttributes.supervisionStrategy(Supervision.restartingDecider)
+        .and(ActorAttributes.logLevels(onElement = Logging.ErrorLevel)))
+      val stream2 = source.via(flow2)
+        .runWith(Sink.ignore)
+      stream2.awaitOnCompleteAndPrint
     }
 
+    // mapAsync / mapAsyncUnordered / scanAsync - retry/supervision strategies
   }
-
 }
 
 object FutureExtensions {
+
   implicit class FutureLocalOps[T](val fut: Future[T]) extends AnyVal {
     def awaitOnCompleteAndPrint = awaitOnComplete(returnValue => println("Stream completed with value: " + returnValue))
+
     def awaitOnComplete[U](f: Try[T] => U, timeout: Duration = 10 seconds): Unit = {
       fut.onComplete(f)
       Await.ready(fut, timeout)
     }
   }
+
 }
