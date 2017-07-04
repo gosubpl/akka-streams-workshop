341d0566caf8a3eadf8bcfbdb8eb26a1d77bdb89
gosubpl <github@gosub.pl>
2017-07-04 22:33:15 +0200

    GraphSpec is for custom graphs

---
+++ src/test/scala/pl/gosub/akka_streams/workshop/GraphSpec.scala
@@ -0,0 +1,16 @@
+package pl.gosub.akka_streams.workshop
+
+import akka.actor.ActorSystem
+import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.{ActorMaterializer, OverflowStrategy, ThrottleMode}
+import org.scalatest.FreeSpec
+
+import scala.concurrent.Await
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+// for graphs -> one example just flow the other with integrated source
+// which will need this more complex builder
+class GraphSpec extends FreeSpec {
+
+}
